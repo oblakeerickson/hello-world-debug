7dde86c0cdfe7cb6bc138550d28a066699f9ee7a
oblakeerickson <o.blakeerickson@gmail.com>
2015-01-24 07:12:56 -0700

    add debug conditional

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            ShowDebugInfo("my message");
+        }
+
+        [System.Diagnostics.Conditional("DEBUG")]
+        static void ShowDebugInfo(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
